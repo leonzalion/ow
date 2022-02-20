57c5793474d84914216df609ba260cfd9cf157f4
Leon Si <contact@leonzalion.com>
2022-02-19 20:15:28 -0500

    fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
-import { execaCommandSync as exec } from 'execa';
-import { copyPackageFiles, rmDist } 'lion-system';
+import { execaCommandSync as exec } from "execa";
+import { copyPackageFiles, rmDist } from "lion-system";
 
 rmDist();
-exec('tsc');
+exec("tsc");
 copyPackageFiles();
